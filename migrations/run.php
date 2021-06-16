e4ca31d08a65cdab5a1451ecb56941bbcc09180d
Hugo Peixoto <hugo.peixoto@gmail.com>
2021-06-16 03:59:31 +0100

    Fix migration runner

--- migrations/run.php
+++ migrations/run.php
@@ -10,7 +10,7 @@ db_query("CREATE TABLE IF NOT EXISTS schema_migrations(name TEXT)");
 foreach ($migrations as $migration) {
   $basename = basename($migration);
 
-  $exists = mysqli_fetch_assoc(db_query("SELECT 1 FROM schema_migrations WHERE name = '" . db_escape_string($basename) . "'")) != NULL;
+  $exists = mysqli_fetch_assoc(db_query("SELECT * FROM schema_migrations WHERE name = '" . db_escape_string($basename) . "'")) != NULL;
 
   if (!$exists) {
     echo "Running migration $basename\n";
@@ -18,6 +18,11 @@ foreach ($migrations as $migration) {
     if (!db_multi_query(file_get_contents($migration))) {
       echo "Error running migration $basename\n";
       exit;
+    }
+
+    mysqli_use_result($db);
+    while (mysqli_more_results($db)) {
+      mysqli_next_result($db);
     }
 
     db_query("INSERT INTO schema_migrations (name) VALUES ('" . db_escape_string($basename) . "')");
