812eba9af2e007ed558994b94d53547c2bf67476
Hugo Peixoto <hugo.peixoto@gmail.com>
2021-06-24 11:09:09 +0100

    Add tool to recalculate every cached table

---
+++ tools/recalc-everything.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../common.php';
+
+// Recalculate every ladder totals, AF, and SRPR.
+
+foreach ([1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 13, 14, 15, 16, 17, 18] as $ladder_id) {
+  recalc_ladder_totals($ladder_id);
+  recalc_af($ladder_id);
+  recalc_srpr($ladder_id);
+}
